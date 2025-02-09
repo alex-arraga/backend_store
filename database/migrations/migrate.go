f9e952dc538582bdb02d7c4c1c5dc82558256ac9
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-09 01:38:00 -0300

    wip

--- database/migrations/migrate.go
+++ database/migrations/migrate.go
@@ -10,7 +10,7 @@ import (
 func migrate(models ...interface{}) {
 	for _, model := range models {
 		if err := connection.DB.AutoMigrate(model); err != nil {
-			log.Fatalf("Error executing migration: %v", err)
+			log.Fatalf("Error executing migration: %d", err)
 		}
 	}
 	log.Printf("Migrations successfully")
