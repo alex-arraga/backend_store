43435b930ac5a6e8e5ec39241af7850509316bdf
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-09 01:41:56 -0300

    Improve fields of user model

--- database/gorm_models/user_db_model.go
+++ database/gorm_models/user_db_model.go
@@ -11,8 +11,8 @@ import (
 type User struct {
 	ID        uuid.UUID `gorm:"type:uuid;primaryKey"`
 	Name      string    `gorm:"size:255;not null"`
-	Email     string    `gorm:"unique;not null"`
-	Password  *string   `gorm:"default:null"`
+	Email     string    `gorm:"size:255;unique;not null"`
+	Password  *string   `gorm:"size:255;default:null"`
 	Role      string    `gorm:"size:10;not null;default:user"`
 	CreatedAT time.Time `gorm:"autoCreateTime"`
 	UpdatedAT time.Time `gorm:"autoUpdateTime"`
