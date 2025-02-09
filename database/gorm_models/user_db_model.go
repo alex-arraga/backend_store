d03364506e5fc0026e66fb6284bce854dd0a1a98
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-09 01:36:03 -0300

    Enum roles

--- database/gorm_models/user_db_model.go
+++ database/gorm_models/user_db_model.go
@@ -1,6 +1,7 @@
 package gorm_models
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -31,4 +32,19 @@ func (u *User) HashPassword(password string) error {
 func (u *User) CheckPassword(password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(*u.Password), []byte(password))
 	return err == nil
+}
+
+// Enum of roles
+var validRoles = map[string]bool{
+	"user":   true,
+	"seller": true,
+	"admin":  true,
+}
+
+// Checks if the user role exist and set them
+func (u *User) SetRole(role string) error {
+	if !validRoles[role] {
+		return fmt.Errorf("invalid role: %s", role)
+	}
+	return nil
 }
