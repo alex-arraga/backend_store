3836317a2884bd8b2786645e8142ea13e6a4d7f6
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-13 16:07:04 -0300

    Repos: Get user by id

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -7,6 +7,7 @@ import (
 )
 
 type UserRepository interface {
+	GetAllUsers() ([]*gorm_models.User, error)
 	CreateUser(user *gorm_models.User) error
 	GetUserByID(id string) (*gorm_models.User, error)
 }
@@ -31,4 +32,15 @@ func (repo *RepoConnection) GetUserByID(id string) (*gorm_models.User, error) {
 		return nil, result.Error
 	}
 	return &user, nil
+}
+
+func (repo *RepoConnection) GetAllUsers() ([]*gorm_models.User, error) {
+	var users []*gorm_models.User
+
+	result := repo.db.Find(&users)
+	if result.Error != nil {
+		return []*gorm_models.User{}, result.Error
+	}
+
+	return users, nil
 }
