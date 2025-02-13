3c404e30b6bc026e618949330ff71184011f87b0
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-13 16:50:34 -0300

    wip

--- services/user_services.go
+++ services/user_services.go
@@ -14,6 +14,7 @@ import (
 type UserService interface {
 	CreateUser(user *models.User) error
 	GetUserByID(id string) (*models.User, error)
+	GetAllUsers() ([]models.User, error)
 }
 
 type UserServiceImpl struct {
