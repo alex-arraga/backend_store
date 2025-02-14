20c40bfe3207bdb7070cf3f6bafe0cc36c093eaf
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-14 10:11:19 -0300

    wip

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -20,8 +20,15 @@ func newUserRepo(db *gorm.DB) UserRepository {
 	return &RepoConnection{db: db}
 }
 
-func (repo *RepoConnection) CreateUser(user *gorm_models.User) error {
-	return repo.db.Create(user).Error
+func (repo *RepoConnection) GetAllUsers() ([]gorm_models.User, error) {
+	var users []gorm_models.User
+
+	result := repo.db.Find(&users)
+	if result.Error != nil {
+		return []gorm_models.User{}, result.Error
+	}
+
+	return users, nil
 }
 
 func (repo *RepoConnection) GetUserByID(id string) (*gorm_models.User, error) {
@@ -34,13 +41,6 @@ func (repo *RepoConnection) GetUserByID(id string) (*gorm_models.User, error) {
 	return &user, nil
 }
 
-func (repo *RepoConnection) GetAllUsers() ([]gorm_models.User, error) {
-	var users []gorm_models.User
-
-	result := repo.db.Find(&users)
-	if result.Error != nil {
-		return []gorm_models.User{}, result.Error
-	}
-
-	return users, nil
+func (repo *RepoConnection) CreateUser(user *gorm_models.User) error {
+	return repo.db.Create(user).Error
 }
