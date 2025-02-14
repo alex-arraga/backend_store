efdabe9d8959640e6948f79c58992cc739b2de74
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-14 10:20:25 -0300

    wip

--- services/user_services.go
+++ services/user_services.go
@@ -12,9 +12,9 @@ import (
 )
 
 type UserService interface {
-	CreateUser(user *models.User) error
-	GetUserByID(id string) (*models.User, error)
 	GetAllUsers() ([]models.User, error)
+	GetUserByID(id string) (*models.User, error)
+	CreateUser(user *models.User) error
 }
 
 type UserServiceImpl struct {
@@ -23,40 +23,6 @@ type UserServiceImpl struct {
 
 func NewUserService(repo repositories.UserRepository) UserService {
 	return &UserServiceImpl{repo: repo}
-}
-
-func (s *UserServiceImpl) CreateUser(userReq *models.User) error {
-	// Hashing password
-	hashedPassword, err := utils.HashPassword(userReq.Password)
-	if err != nil {
-		return fmt.Errorf("error hashing password: %d", err)
-	}
-
-	user := &gorm_models.User{
-		ID:       uuid.New(),
-		Name:     userReq.Name,
-		Email:    userReq.Email,
-		Password: &hashedPassword,
-	}
-
-	// Send data to repository
-	return s.repo.CreateUser(user)
-}
-
-func (s *UserServiceImpl) GetUserByID(id string) (*models.User, error) {
-	userDB, err := s.repo.GetUserByID(id)
-	if err != nil {
-		return nil, fmt.Errorf("couldn't get user: %d", err)
-	}
-
-	userReq := models.User{
-		ID:       userDB.ID,
-		Name:     userDB.Name,
-		Email:    userDB.Email,
-		Password: *userDB.Password,
-	}
-
-	return &userReq, nil
 }
 
 func (s *UserServiceImpl) GetAllUsers() ([]models.User, error) {
@@ -79,4 +45,38 @@ func (s *UserServiceImpl) GetAllUsers() ([]models.User, error) {
 	}
 
 	return allUsers, nil
+}
+
+func (s *UserServiceImpl) GetUserByID(id string) (*models.User, error) {
+	userDB, err := s.repo.GetUserByID(id)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't get user: %d", err)
+	}
+
+	userReq := models.User{
+		ID:       userDB.ID,
+		Name:     userDB.Name,
+		Email:    userDB.Email,
+		Password: *userDB.Password,
+	}
+
+	return &userReq, nil
+}
+
+func (s *UserServiceImpl) CreateUser(userReq *models.User) error {
+	// Hashing password
+	hashedPassword, err := utils.HashPassword(userReq.Password)
+	if err != nil {
+		return fmt.Errorf("error hashing password: %d", err)
+	}
+
+	user := &gorm_models.User{
+		ID:       uuid.New(),
+		Name:     userReq.Name,
+		Email:    userReq.Email,
+		Password: &hashedPassword,
+	}
+
+	// Send data to repository
+	return s.repo.CreateUser(user)
 }
