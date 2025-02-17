f5bfe4cef2d2ba9ff6c3df0e307a20f17b5e0daa
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-17 12:38:59 -0300

    Models: UpdateUserRequest

--- models/user_model.go
+++ models/user_model.go
@@ -6,9 +6,15 @@ import (
 
 // TODO: The API would cans update the user rol
 type User struct {
-	ID uuid.UUID `json:"id"`
-	// Role     string    `json:"role"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID       uuid.UUID `json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"password"`
+}
+
+type UpdateUserRequest struct {
+	Name     *string `json:"name,omitempty"`
+	Email    *string `json:"email,omitempty"`
+	Password *string `json:"password,omitempty"`
+	Role     *string `json:"role,omitempty"` // Si decides permitir cambiar roles
 }
