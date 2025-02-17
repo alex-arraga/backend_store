d9114173c71c902c746ef07cb04a4034c4d85502
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-17 12:41:23 -0300

    wip

--- models/user_model.go
+++ models/user_model.go
@@ -12,7 +12,7 @@ type User struct {
 	Password string    `json:"password"`
 }
 
-type UpdateUserRequest struct {
+type UpdateUser struct {
 	Name     *string `json:"name,omitempty"`
 	Email    *string `json:"email,omitempty"`
 	Password *string `json:"password,omitempty"`
