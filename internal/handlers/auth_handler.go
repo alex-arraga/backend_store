9c482a2b994c6305599d1293e06c2719dbef25c5
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-25 18:24:23 -0300

    Auth: AuthLoginHandler to begin auth

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/alex-arraga/backend_store/pkg/jsonutil"
+	"github.com/alex-arraga/backend_store/pkg/logger"
 	"github.com/go-chi/chi/v5"
 	"github.com/markbates/goth/gothic"
 )
@@ -28,4 +30,18 @@ func GetAuthCallback(w http.ResponseWriter, r *http.Request) {
 
 	// Redirect when auth successfully
 	http.Redirect(w, r, "http://localhost:5173/", http.StatusFound)
+}
+
+func AuthLoginHandler(w http.ResponseWriter, r *http.Request) {
+	provider := chi.URLParam(r, "provider")
+	if provider == "" {
+		logger.UseLogger().Error().Msg("Missing provider in OAuth process")
+		jsonutil.RespondError(w, http.StatusBadRequest, "Missing provider")
+		return
+	}
+
+	logger.UseLogger().Info().Msgf("Starting authentication with %s ", provider)
+
+	// Automatically redirects to Google for user authentication
+	gothic.BeginAuthHandler(w, r)
 }
