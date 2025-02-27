f06f2bdd835f254442d0aebfc8d8e2f99b8c75f0
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-27 19:54:53 -0300

    wip

--- internal/routes/auth_routes.go
+++ internal/routes/auth_routes.go
@@ -6,18 +6,27 @@ import (
 	"github.com/go-chi/chi/v5"
 
 	"github.com/alex-arraga/backend_store/internal/handlers"
+	"github.com/alex-arraga/backend_store/pkg/jsonutil"
 )
 
 func authRoutes() chi.Router {
 	r := chi.NewRouter()
 
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		jsonutil.RespondJSON(w, http.StatusOK, "Auth routes mounted", struct{}{})
+	})
+
 	// Inits authentication process with Gothic
-	r.Get("/{provider}/login}", func(w http.ResponseWriter, r *http.Request) {
-		handlers.GetAuthCallback(w, r)
+	r.Get("/{provider}", func(w http.ResponseWriter, r *http.Request) {
+		jsonutil.RespondJSON(w, http.StatusOK, "Provider route", struct{}{})
+	})
+
+	r.Get("/{provider}/login", func(w http.ResponseWriter, r *http.Request) {
+		handlers.AuthLoginHandler(w, r)
 	})
 
 	// Receives Google response and get the authenticated user
-	r.Get("/{provider}/callback}", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/{provider}/callback", func(w http.ResponseWriter, r *http.Request) {
 		handlers.GetAuthCallback(w, r)
 	})
 
