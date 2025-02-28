e794a3e5a59be06be79aefa98c3a8333089bfbdf
alex-arraga <alex-arraga@users.noreply.github.com>
2025-02-28 19:01:33 -0300

    Auth: Improve auth config

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -11,29 +11,85 @@ import (
 	"github.com/alex-arraga/backend_store/pkg/logger"
 )
 
-// TODO: secretKey and appEnv should be held from an .env file
+type Opts struct {
+	GoogleClientID     string
+	GoogleClientSecret string
+	CallbackURL        string
+	SecretKey          string
+	MaxAge             int
+	HttpOnly           bool
+	SecureMode         bool
+}
 
-const (
-	secretKey = "randomString"
-	MaxAge    = 86400 * 30 // 30 days
-	IsProd    = false
-)
+type Config struct {
+	opts Opts
+}
 
-func NewAuth() {
+func loadOptions() Opts {
+	// Google opts
 	googleClientID := os.Getenv("GOOGLE_CLIENT_ID")
 	googleClientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
 	callbackURL := os.Getenv("GOOGLE_CALLBACK_URL")
 
-	if googleClientID == "" || googleClientSecret == "" {
-		logger.UseLogger().Fatal().Msg("Missing required environment variables: GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET")
+	if googleClientID == "" || googleClientSecret == "" || callbackURL == "" {
+		logger.UseLogger().Fatal().Msg("Missing required environment variables: GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, CALLBACK_URL")
+		panic("Missing required environment variables: GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, CALLBACK_URL")
 	}
 
-	store := sessions.NewCookieStore([]byte(secretKey))
+	// App enviroment opts
+	var isProd bool
+	var httpOnly bool
+
+	secretKey := os.Getenv("SECRET_KEY")
+	appEnv := os.Getenv("APP_ENV")
+
+	if secretKey == "" || appEnv == "" {
+		logger.UseLogger().Fatal().Msg("Missing required enviroment variables: SECRET_KEY or APP_ENV")
+		panic("Missing required environment variables: SECRET_KEY or APP_ENV")
+	}
+
+	// If application are in "dev" enviroment, isProd will be false, otherwise will be true
+	if appEnv == "dev" {
+		isProd = false
+	} else {
+		isProd = true
+	}
+
+	// If application are in "dev" enviroment, httpOnly will be true, otherwise will be false
+	if isProd {
+		httpOnly = false
+	} else {
+		httpOnly = true
+	}
+
+	return Opts{
+		GoogleClientID:     googleClientID,
+		GoogleClientSecret: googleClientSecret,
+		CallbackURL:        callbackURL,
+		MaxAge:             86400 * 30,
+		SecretKey:          secretKey,
+		SecureMode:         isProd,
+		HttpOnly:           httpOnly,
+	}
+}
+
+func newConfig() *Config {
+	o := loadOptions()
+
+	return &Config{
+		opts: o,
+	}
+}
+
+func NewAuth() {
+	config := newConfig()
+
+	store := sessions.NewCookieStore([]byte(config.opts.SecretKey))
 	store.Options = &sessions.Options{
 		Path:     "/",
-		MaxAge:   MaxAge,
-		HttpOnly: true,
-		Secure:   IsProd,
+		MaxAge:   config.opts.MaxAge,
+		HttpOnly: config.opts.HttpOnly,
+		Secure:   config.opts.SecureMode,
 	}
 
 	// Assign session storage to Gothic
@@ -41,6 +97,6 @@ func NewAuth() {
 
 	// Config OAuth providers
 	goth.UseProviders(
-		google.New(googleClientID, googleClientSecret, callbackURL, "email", "profile"),
+		google.New(config.opts.GoogleClientID, config.opts.GoogleClientSecret, config.opts.CallbackURL, "email", "profile"),
 	)
 }
