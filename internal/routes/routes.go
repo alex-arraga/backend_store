3ce53fed81da4fede9983cc6b2a7fac0658a0841
alex-arraga <alex-arraga@users.noreply.github.com>
2025-03-05 02:46:15 -0300

    wip

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -11,8 +11,6 @@ func MountRoutes(services *services.ServicesContainer) chi.Router {
 
 	v1Router := chi.NewRouter()
 	v1Router.Mount("/user", userRoutes(services.UserSrv))
-
-	// Auth route
 	v1Router.Mount("/auth", authRoutes(services.AuthSrv))
 
 	r.Mount("/v1", v1Router)
