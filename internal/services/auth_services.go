4ccd7b8a01a895b146694a28ae71dd0e14abf5c5
alex-arraga <alex-arraga@users.noreply.github.com>
2025-03-05 02:18:14 -0300

    Services: Funcs added in Auth Services

---
+++ internal/services/auth_services.go
@@ -0,0 +1,85 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/alex-arraga/backend_store/internal/database/gorm_models"
+	"github.com/alex-arraga/backend_store/internal/models"
+	"github.com/alex-arraga/backend_store/internal/repositories"
+	"github.com/google/uuid"
+	"github.com/markbates/goth"
+)
+
+type AuthServices interface {
+	_RegisterWithEmailAndPassword(userReq *models.User) (*models.UserResponse, error)
+	_RegisterWithOAuth(user goth.User) (*models.UserResponse, error)
+}
+
+type authServiceImpl struct {
+	repo repositories.AuthRepository
+}
+
+func newAuthService(repo repositories.AuthRepository) AuthServices {
+	return &authServiceImpl{repo: repo}
+}
+
+func (s *authServiceImpl) _RegisterWithEmailAndPassword(userReq *models.User) (*models.UserResponse, error) {
+	u := &gorm_models.User{
+		ID:       uuid.New(),
+		FullName: userReq.FullName,
+		Email:    userReq.Email,
+		// EmailVerified: false,
+		PasswordHash: &userReq.PasswordHash,
+		// Provider: "local",
+	}
+
+	// Send data to repository
+	userDB, err := s.repo.RegisterUser(u)
+	if err != nil {
+		return nil, fmt.Errorf("error creating user: %w", err)
+	}
+
+	userResp := &models.UserResponse{
+		ID:        userDB.ID,
+		FullName:  userDB.FullName,
+		Email:     userDB.Email,
+		Role:      userDB.Role,
+		Provider:  userDB.Provider,
+		AvatarURL: userDB.AvatarURL,
+	}
+
+	return userResp, nil
+}
+
+func (s *authServiceImpl) _RegisterWithOAuth(user goth.User) (*models.UserResponse, error) {
+	fullName := fmt.Sprint(user.Name + " " + user.LastName)
+
+	// Converts goth.User (OAuth) to gorm_model.User, in order to be able to send it to the database
+	u := gorm_models.User{
+		ID:            uuid.New(),
+		FullName:      fullName,
+		Email:         user.Email,
+		EmailVerified: true,
+		Provider:      user.Provider,
+		ProviderID:    &user.UserID,
+		AvatarURL:     &user.AvatarURL,
+	}
+
+	// Send data to database
+	userDB, err := s.repo.RegisterUser(&u)
+	if err != nil {
+		return &models.UserResponse{}, fmt.Errorf("")
+	}
+
+	// Converts gorm_model.User (database model) to models.UserResponse, in order to be able to send it to the client
+	userResponse := models.UserResponse{
+		ID:        userDB.ID,
+		FullName:  userDB.FullName,
+		Email:     userDB.Email,
+		Role:      userDB.Role,
+		Provider:  userDB.Provider,
+		AvatarURL: userDB.AvatarURL,
+	}
+
+	return &userResponse, nil
+}
--- internal/services/services.go
+++ internal/services/services.go
@@ -3,11 +3,13 @@ package services
 import "github.com/alex-arraga/backend_store/internal/repositories"
 
 type ServicesContainer struct {
+	AuthSrv AuthServices
 	UserSrv UserService
 }
 
 func LoadServices(repo *repositories.RepositoryContainer) *ServicesContainer {
 	return &ServicesContainer{
+		AuthSrv: newAuthService(repo.AuthRepo),
 		UserSrv: NewUserService(repo.UserRepo),
 	}
 }
