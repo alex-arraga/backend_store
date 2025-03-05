775d298c997eb9bd8949b130d6e6b505fc360293
alex-arraga <alex-arraga@users.noreply.github.com>
2025-03-05 02:00:33 -0300

    wip

--- internal/repositories/auth_repository.go
+++ internal/repositories/auth_repository.go
@@ -1,10 +1,23 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"github.com/alex-arraga/backend_store/internal/database/gorm_models"
+	"gorm.io/gorm"
+)
 
 type AuthRepository interface {
+	RegisterUser(user *gorm_models.User) (*gorm_models.User, error)
+	// LoginUser(user *gorm_models.User) (*gorm_models.User, error)
 }
 
 func newAuthRepo(db *gorm.DB) AuthRepository {
 	return &RepoConnection{db: db}
+}
+
+func (repo *RepoConnection) RegisterUser(user *gorm_models.User) (*gorm_models.User, error) {
+	if result := repo.db.Create(user); result.Error != nil {
+		return nil, result.Error
+	}
+
+	return user, nil
 }
