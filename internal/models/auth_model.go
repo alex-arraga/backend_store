2dbd680d2e1a94c85386ec62f38785e2f895237b
alex-arraga <alex-arraga@users.noreply.github.com>
2025-03-10 18:02:28 -0300

    wip

--- internal/models/auth_model.go
+++ internal/models/auth_model.go
@@ -1,8 +1,6 @@
 package models
 
-import "github.com/golang-jwt/jwt/v5"
-
 type AuthResponse struct {
 	User  UserResponse `json:"user"`
-	Token jwt.Token    `json:"token"`
+	Token string       `json:"token"`
 }
