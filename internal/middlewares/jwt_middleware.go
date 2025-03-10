2bc310137da06350fd6430a90e14cd90412115ea
alex-arraga <alex-arraga@users.noreply.github.com>
2025-03-10 20:04:02 -0300

    Middlewares: Create JWT middleware

---
+++ internal/middlewares/jwt_middleware.go
@@ -0,0 +1,41 @@
+package middlewares
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/alex-arraga/backend_store/pkg/auth"
+)
+
+type contextKey string
+
+const UserIDKey contextKey = "user_id"
+
+func JWTAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Missing Authorization header", http.StatusUnauthorized)
+			return
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
+		}
+
+		tokenStr := parts[1]
+
+		// Validate token and get claims
+		claims, err := auth.ValidateJWT(tokenStr)
+		if err != nil {
+			http.Error(w, "Invalid Token", http.StatusUnauthorized)
+			return
+		}
+
+		// Inyect user ID in the context
+		ctx := context.WithValue(r.Context(), UserIDKey, claims.UserID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
