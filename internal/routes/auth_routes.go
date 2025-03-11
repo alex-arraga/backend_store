157d9a92111c828ac2dd8407ef432c34faaedb06
alex-arraga <alex-arraga@users.noreply.github.com>
2025-03-11 18:13:14 -0300

    wip

--- internal/routes/auth_routes.go
+++ internal/routes/auth_routes.go
@@ -11,28 +11,25 @@ import (
 
 // Public auth routes
 func loadPublicAuthRoutes(r chi.Router, as services.AuthServices) {
+	// Path of group: /v1/auth
 	r.Route("/auth", func(r chi.Router) {
-		// Path of group: /v1/auth
-		r.Group(func(r chi.Router) {
-			// Register using email and password
-			r.Post("/register", func(w http.ResponseWriter, r *http.Request) {
-				handlers.RegisterUserWithEmailHandler(w, r, as)
-			})
+		r.Post("/register", func(w http.ResponseWriter, r *http.Request) {
+			handlers.RegisterUserWithEmailHandler(w, r, as)
+		})
 
-			// Login using email and password
-			r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
-				handlers.LoginUserWithEmailHandler(w, r, as)
-			})
+		// Login using email and password
+		r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
+			handlers.LoginUserWithEmailHandler(w, r, as)
+		})
 
-			// Starts OAuth login
-			r.Get("/{provider}/login", func(w http.ResponseWriter, r *http.Request) {
-				handlers.BeginAuthLoginHandler(w, r)
-			})
+		// Starts OAuth login
+		r.Get("/{provider}/login", func(w http.ResponseWriter, r *http.Request) {
+			handlers.BeginAuthLoginHandler(w, r)
+		})
 
-			// Receives Google response and get the authenticated user
-			r.Get("/{provider}/callback", func(w http.ResponseWriter, r *http.Request) {
-				handlers.GetAuthCallbackHandler(w, r, as)
-			})
+		// Receives Google response and get the authenticated user
+		r.Get("/{provider}/callback", func(w http.ResponseWriter, r *http.Request) {
+			handlers.GetAuthCallbackHandler(w, r, as)
 		})
 	})
 }
