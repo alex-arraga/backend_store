b7f273d63a00e45ee9100e5b8fde177d0c63b693
alex-arraga <alex-arraga@users.noreply.github.com>
2025-03-11 18:15:08 -0300

    wip

--- internal/routes/user_routes.go
+++ internal/routes/user_routes.go
@@ -9,25 +9,23 @@ import (
 	"github.com/alex-arraga/backend_store/internal/services"
 )
 
+// Path of routes: /v1/user
 func loadProtectedUserRoutes(r chi.Router, us services.UserService) {
 	r.Route("/user", func(r chi.Router) {
-		// Path of group: /v1/user
-		r.Group(func(r chi.Router) {
-			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-				handlers.GetAllUsersHandler(w, r, us)
-			})
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			handlers.GetAllUsersHandler(w, r, us)
+		})
 
-			r.Get("/{userID}", func(w http.ResponseWriter, r *http.Request) {
-				handlers.GetUserByIDHandler(w, r, us)
-			})
+		r.Get("/{userID}", func(w http.ResponseWriter, r *http.Request) {
+			handlers.GetUserByIDHandler(w, r, us)
+		})
 
-			r.Put("/{targetUserID}", func(w http.ResponseWriter, r *http.Request) {
-				handlers.UpdateUserHandler(w, r, us)
-			})
+		r.Put("/{targetUserID}", func(w http.ResponseWriter, r *http.Request) {
+			handlers.UpdateUserHandler(w, r, us)
+		})
 
-			r.Delete("/{userID}", func(w http.ResponseWriter, r *http.Request) {
-				handlers.DeleteUserHandler(w, r, us)
-			})
+		r.Delete("/{userID}", func(w http.ResponseWriter, r *http.Request) {
+			handlers.DeleteUserHandler(w, r, us)
 		})
 	})
 }
