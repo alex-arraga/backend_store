5c42ee38308297270458a80806450b253569421c
alex-arraga <alex-arraga@users.noreply.github.com>
2025-03-13 20:32:06 -0300

    lc

--- internal/repositories/repositories.go
+++ internal/repositories/repositories.go
@@ -8,14 +8,14 @@ type RepoConnection struct {
 	db *gorm.DB
 }
 type RepositoryContainer struct {
-	UserRepo UserRepository
 	AuthAccountRepo AuthAccountRepository
+	UserRepo        UserRepository
 }
 
 // Connects db with the all repositories
 func LoadRepositories(db *gorm.DB) *RepositoryContainer {
 	return &RepositoryContainer{
-		UserRepo: newUserRepo(db),
 		AuthAccountRepo: newAuthAccountRepo(db),
+		UserRepo:        newUserRepo(db),
 	}
 }
